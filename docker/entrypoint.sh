fcf00b74b24cc57467d38cf21d569f979b778944
Artyom Osepyan <art@osepyan.ru>
2026-03-14 19:51:17 +0300

    feat: created project

---
+++ docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+composer install
+
+if [[ -f .env ]]; then
+  echo ".env already exists"
+else
+  cp .env.example .env
+  php artisan key:generate
+fi
+
+php artisan migrate --force
+chmod -R 777 storage
